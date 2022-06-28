39dc62241de0abff423cf754d248f07a0c1c53b1
Jonathan Rudd <jonathanrudd3@gmail.com>
2022-06-28 21:25:19 +0900

    works but no proper css

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,13 @@
+class AnswersController < ApplicationController
+  def answer
+    # compute an instance variable @answer for the view to display
+    @answer = ''
+
+    # if params[:answer]
+    # @answer = params[:answer]
+    # end
+    # display input
+
+    # retrieve input from params and display depending on what it is
+  end
+end
